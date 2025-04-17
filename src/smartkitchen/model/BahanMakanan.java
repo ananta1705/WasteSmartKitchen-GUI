e9642a819f5df0578ad12ae80a8f870d21781097
ANANTA PUTI MAHARANI <your@example.com>
2025-04-17 23:23:54 +0700

    first commit

---
+++ src/smartkitchen/model/BahanMakanan.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package smartkitchen.model;
+
+/**
+ *
+ * @author ASUS
+ */
+public class BahanMakanan {
+    private String nama;
+    private int jumlah;
+    private String tanggalKadaluarsa;
+
+    public BahanMakanan(String nama, int jumlah, String tanggalKadaluarsa) {
+        this.nama = nama;
+        this.jumlah = jumlah;
+        this.tanggalKadaluarsa = tanggalKadaluarsa;
+    }
+
+    // Getter & Setter
+}
